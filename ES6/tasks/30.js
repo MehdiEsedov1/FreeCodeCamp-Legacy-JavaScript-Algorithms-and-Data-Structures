44d812e389a21a9a935af263b52f488a9fc872b9
MehdiEsedov1 <mehdiesedof@gmail.com>
2024-12-17 11:19:02 +0400

    Add new lessons

---
+++ ES6/tasks/30.js
@@ -0,0 +1,2 @@
+const myName = "Mehdi";
+const myStr = "My name is " + myName + " and I am well!";
---
+++ ES6/tasks/38.js
@@ -0,0 +1,16 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const formedString =
+  "My " +
+  myNoun +
+  " is " +
+  myAdjective +
+  " and it can " +
+  myVerb +
+  " very " +
+  myAdverb;
+
+const wordBlanks = formedString;
